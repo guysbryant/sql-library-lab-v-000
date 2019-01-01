e39a5cc72d6e0f4262f8310d77439a82c5bcf440
Guy Bryant <guy.s.bryant@gmail.com>
2019-01-01 14:55:55 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -29,7 +29,6 @@ def select_series_title_with_most_human_characters
   "SELECT series.title 
     FROM series JOIN characters ON characters.series_id = series.id 
     WHERE characters.species = 'human'
-    GROUP BY characters.species
     ORDER BY COUNT(characters.species);"
 end
 
