700ac999425a6f4f9c7f71c8190443120c1845db
Guy Bryant <guy.s.bryant@gmail.com>
2019-01-01 15:11:09 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -30,7 +30,7 @@ def select_series_title_with_most_human_characters
     FROM series JOIN characters ON characters.series_id = series.id 
       Where characters.species = 'human'
       GROUP BY series.title
-      ORDER BY COUNT(characters.species);"
+      ORDER BY COUNT(characters.species) DESC;"
 end
 
 def select_character_names_and_number_of_books_they_are_in
