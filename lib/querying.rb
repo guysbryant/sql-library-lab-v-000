7bffd79063fde429d41af8aab7fb96e45275add5
Guy Bryant <guy.s.bryant@gmail.com>
2019-01-01 15:32:30 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -37,5 +37,5 @@ def select_character_names_and_number_of_books_they_are_in
   "SELECT characters.name, COUNT(*)
     FROM characters JOIN character_books ON character_books.character_id = characters.id 
       GROUP BY character_books.book_id 
-        ORDER BY COUNT(DISTINCT character_books.book_id);"
+        ;"
 end
