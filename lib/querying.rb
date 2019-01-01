3a7ac069e343b1db63889953b09c3f8921f1c97b
Guy Bryant <guy.s.bryant@gmail.com>
2019-01-01 14:42:13 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -14,7 +14,8 @@ end
 
 def select_value_and_count_of_most_prolific_species
   "SELECT characters.species, COUNT(characters.species) AS species_count
-    FROM characters;"
+    FROM characters
+    ORDER BY species_count;"
 end
 
 def select_name_and_series_subgenres_of_authors
