ea8c5c00620e783caa52e4e755fc0bc2021daac5
Guy Bryant <guy.s.bryant@gmail.com>
2019-01-01 13:28:39 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,6 +1,6 @@
 def select_books_titles_and_years_in_first_series_order_by_year
   "SELECT books.title, books.year 
-    FROM books JOIN series ON series.book_id = books.id 
+    FROM books JOIN series ON series.id = books.series_id 
     WHERE series.id =1
     GROUP BY books.year;"
 end
