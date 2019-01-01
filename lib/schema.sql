36ccc0845a1f97ca855528043d66ef48f13cacea
Guy Bryant <guy.s.bryant@gmail.com>
2019-01-01 13:05:25 +0000

    Automatically backed up by Learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -2,3 +2,4 @@ CREATE TABLE series (id INTEGER PRIMARY KEY, title TEXT, author_id INTEGER, sub_
 CREATE TABLE authors (id INTEGER PRIAMRY KEY, name TEXT);
 CREATE TABLE books (id INTEGER PRIMARY KEY, title TEXT, year INTEGER, series_id INTEGER);
 CREATE TABLE characters (id INTEGER PRIMARY KEY, name TEXT, motto TEXT, species TEXT, author_id INTEGER, series_id INTEGER);
+CREATE TABLE character_books (id INTEGER PRIMARY KEY, character_id INTEGER, book_id INTEGER);
