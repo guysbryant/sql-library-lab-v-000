6dc12ac6f53e10b06af6a829d2005286da9f2fd8
Guy Bryant <guy.s.bryant@gmail.com>
2019-01-01 13:19:20 +0000

    Automatically backed up by Learn

--- lib/update.sql
+++ lib/update.sql
@@ -1,4 +1,4 @@
 SELECT id FROM characters AS character_id ORDER BY id DESC LIMIT 1
 UPDATE characters 
 SET characters.species = "Martian"
-WHERE characters.name = character;
+WHERE characters.id = character;
